47a284eecd0175b8eb102ac711bd47bebf3bc032
Anonymous <80660607+numpy-ai@users.noreply.github.com>
2022-03-03 22:48:39 +0900

    2022-03-03 #3
    제목과 일부 문장을 영어로 변경, 자신이 설정한 초가 출력되지 않는 문제를 수정

--- shutdown.py
+++ shutdown.py
@@ -2,31 +2,35 @@ import os
 from tkinter import*
 
 tk = Tk()
-tk.geometry("250x250")
-tk.title("시간 지정 셧다운(여러 가지 추가 예정)")
+tk.geometry("300x200")
+tk.title("Enter Shutdown Time")
+tk.resizable(False, False)
 
 lab1 = Label(tk)
-lab1['text'] = "희망하는 n초 후의 종료 시간"
+lab1['text'] = "희망하는 시간(Desired time)"
 lab1.pack()
 
 lab2 = Label(tk)
-def result(n) :
-    lab2.config(text = ent1.get() + "초 후에 종료됩니다.")
-
+def result() :
+    lab2.configure(text = "shutdown a " + ent1.get() + "s after")
 def shutdown_time() : 
+    result()
     return os.system("shutdown -s -t " + ent1.get())
 def cancel() :
     return os.system("shutdown -a")
-def restart() : 
+def restart() :
+    result()
     return os.system("shutdown -r -t")
 
 ent1 = Entry(tk)
 ent1.bind("<Return>", result)
+
 ent1.pack()
 lab2.pack()
 
-Button(tk, text = "shutdown_time", command = shutdown_time).place(x = 80, y = 100)
-Button(tk, text = "restart", command = restart).place(x = 100, y = 130)
-Button(tk, text = "cancel", command = cancel).place(x = 100, y = 160)
+Button(tk, text = "shutdown_time", command = shutdown_time).place(x = 100, y = 70)
+Button(tk, text = "restart", command = restart).place(x = 120, y = 100)
+Button(tk, text = "cancel", command = cancel).place(x = 120, y = 130) 
+
 
 tk.mainloop()
