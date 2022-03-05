0be804118b95b71ee0c77ce804864e80e009e1ce
Anonymous <80660607+numpy-ai@users.noreply.github.com>
2022-03-05 23:01:28 +0900

    2022-03-05 #1
    시간 범위 명시, if문 추가 예정, 버튼 위치 조정

--- shutdown.py
+++ shutdown.py
@@ -3,16 +3,22 @@ from tkinter import*
 
 tk = Tk()
 tk.geometry("300x200")
-tk.title("Enter Shutdown Time")
+tk.title("Shutdown program")
 tk.resizable(False, False)
 
 lab1 = Label(tk)
 lab1['text'] = "희망하는 시간(Time you want)"
 lab1.pack()
+lab3 = Label(tk)
+lab3['text'] = "Range : 0-315360000(10y)"
+lab3.pack()
 
 lab2 = Label(tk)
+ent1 = Entry(tk)
+
 def print_result() :
     lab2.configure(text = "shutdown a " + ent1.get() + "s after")
+
 def shutdown_time() : 
     print_result()
     return os.system("shutdown -s -t " + ent1.get())
@@ -22,14 +28,18 @@ def restart() :
 def cancel() :
     return os.system("shutdown -a")
 
-ent1 = Entry(tk)
-ent1.bind("<Return>", print_result())
+# if ent1.get() <= "315360000" and ent1.get() >= "0" :
+#     pass
+# else :
+#     lab2.configure(text = "Range : 0-315360000")
 
+
+ent1.bind("<Return>", print_result())
 ent1.pack()
 lab2.pack()
 
-Button(tk, text = "shutdown_time", command = shutdown_time).place(x = 100, y = 70)
-Button(tk, text = "restart", command = restart).place(x = 120, y = 100)
-Button(tk, text = "cancel", command = cancel).place(x = 120, y = 130) 
+Button(tk, text = "shutdown time", command = shutdown_time).place(x = 105, y = 90)
+Button(tk, text = "restart", command = restart).place(x = 125, y = 120)
+Button(tk, text = "cancel", command = cancel).place(x = 125, y = 150)
 
 tk.mainloop()
