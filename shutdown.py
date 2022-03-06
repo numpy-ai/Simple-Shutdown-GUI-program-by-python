169eeed3710f01b421f68fb398e19492b8c0ffef
Anonymous <80660607+numpy-ai@users.noreply.github.com>
2022-03-06 23:21:07 +0900

    2022-03-06
    tkinter을 tki로 선언,  lab3을 limit_text_lab으로 변경하여 변수명을 확실하게 함, 현재 범위를 넘어가거나 미치지 못하는 수가 나오면 에러 메시지가 뜨게 할 계획

--- shutdown.py
+++ shutdown.py
@@ -1,45 +1,45 @@
 import os
-from tkinter import*
+import tkinter.messagebox
+import tkinter as tki
+from setuptools import Command
 
-tk = Tk()
+tk = tki.Tk()
 tk.geometry("300x200")
 tk.title("Shutdown program")
 tk.resizable(False, False)
 
-lab1 = Label(tk)
+lab1 = tki.Label(tk)
 lab1['text'] = "희망하는 시간(Time you want)"
 lab1.pack()
-lab3 = Label(tk)
-lab3['text'] = "Range : 0-315360000(10y)"
-lab3.pack()
+limit_text_lab = tki.Label(tk)
+limit_text_lab['text'] = "Range : 0-315360000(10y)"
+limit_text_lab.pack()
 
-lab2 = Label(tk)
-ent1 = Entry(tk)
+lab2 = tki.Label(tk)
+ent1 = tki.Entry(tk)
+ent1.pack()
 
 def print_result() :
-    lab2.configure(text = "shutdown a " + ent1.get() + "s after")
-
-def shutdown_time() : 
-    print_result()
-    return os.system("shutdown -s -t " + ent1.get())
-def restart() :
-    print_result()
-    return os.system("shutdown -r -t")
-def cancel() :
-    return os.system("shutdown -a")
-
-# if ent1.get() <= "315360000" and ent1.get() >= "0" :
-#     pass
-# else :
-#     lab2.configure(text = "Range : 0-315360000")
-
-
-ent1.bind("<Return>", print_result())
-ent1.pack()
+    lab2.configure(text = "shutdown a " + ent1.get() + "m after")
 lab2.pack()
 
-Button(tk, text = "shutdown time", command = shutdown_time).place(x = 105, y = 90)
-Button(tk, text = "restart", command = restart).place(x = 125, y = 120)
-Button(tk, text = "cancel", command = cancel).place(x = 125, y = 150)
+def error_box() :
+    tkinter.messagebox.showerror("Limit Error!", "Range : 0-315360000(10y)")
+
+class main_funtion : 
+    def shutdown_time(self) : 
+        print_result()
+        return os.system("shutdown -s -t " + ent1.get())
+    def restart(self) :
+        print_result()
+        return os.system("shutdown -r -t")
+    def cancel(self) :
+        return os.system("shutdown -a")
+
+Activity_tool = main_funtion()
+
+button1 = tki.Button(tk, text = "shutdown time", command = Activity_tool.shutdown_time).place(x = 105, y = 90)
+button2 = tki.Button(tk, text = "restart", command = Activity_tool.restart).place(x = 125, y = 120)
+button3 = tki.Button(tk, text = "cancel", command = Activity_tool.cancel).place(x = 125, y = 150)
 
 tk.mainloop()
