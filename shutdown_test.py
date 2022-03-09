f2614e1485180064037a4dcbd07d9d2297c58370
Anonymous <80660607+numpy-ai@users.noreply.github.com>
2022-03-09 23:23:33 +0900

    2022-03-09 #1
    변수명을 명확하게 변경, 조건문 추가 예정, get으로 가져오는 값을 정수로 바꿀 수 있는 지 테스트하는 코드 추가 클래스 대문자로 변경

--- shutdown_test.py
+++ shutdown_test.py
@@ -9,46 +9,58 @@ tk.geometry("300x200")
 tk.title("Shutdown program")
 tk.resizable(False, False)
 
-lab1 = tki.Label(tk)
-lab1['text'] = "희망하는 시간(Time you want)"
-lab1.pack()
+top_text = tki.Label(tk)
+top_text['text'] = "희망하는 시간(Time you want)"
+top_text.pack()
 limit_text_lab = tki.Label(tk)
 limit_text_lab['text'] = "Range : 0-315360000(10y)"
 limit_text_lab.pack()
 
-lab2 = tki.Label(tk)
-ent1 = tki.Entry(tk)
-ent1.pack()
+Print_input_time_value = tki.Label(tk)
+input_time_value = tki.Entry(tk)
+input_time_value.insert(tki.INSERT, "Enter Time(s)")   
+input_time_value.pack()
 
 def print_result() :
-    lab2.configure(text = "shutdown a " + ent1.get() + "m after")
-lab2.pack()
+    Print_input_time_value.configure(text = "shutdown a " + input_time_value.get() + "s after")
+Print_input_time_value.pack()
 
 def error_box() :
     tkinter.messagebox.showerror("Limit Error!", "Range : 0-315360000(10y)")
+    
+# if (input_time_value.get() > "315360000" or input_time_value.get() < "0") and len(input_time_value.get()) != 0 :
+#     error_box()
 
-class main_funtion : 
+def get_value(entryWidget):
+    value = entryWidget.get()
+    try:
+        return int(value)
+    except ValueError:
+        return None
+
+
+class Main_funtion : 
     def shutdown_time(self) : 
         print_result()
-        return os.system("shutdown -s -t " + ent1.get())
+        return os.system("shutdown -s -t " + input_time_value.get())
     def restart(self) :
         print_result()
-        return os.system("shutdown -r -t")
+        return os.system("shutdown -r -t " + input_time_value.get())
     def cancel(self) :
         return os.system("shutdown -a")
 
-Activity_tool = main_funtion()
+Activity_tool = Main_funtion()
 
 button1 = tki.Button(tk, text = "shutdown time", command = Activity_tool.shutdown_time).place(x = 105, y = 90)
 button2 = tki.Button(tk, text = "restart", command = Activity_tool.restart).place(x = 125, y = 120)
 button3 = tki.Button(tk, text = "cancel", command = Activity_tool.cancel).place(x = 125, y = 150)
 
-# if ent1.get() > "315360000":
-#     print(len(ent1.get()))
+# if input_time_value.get() > "315360000":
+#     print(len(input_time_value.get()))
 #     error_box()
 
-# if ent1.get() <= "315360000" and ent1.get() >= "0" :
-#     Activity_tool = main_funtion()
+# if input_time_value.get() <= "315360000" and input_time_value.get() >= "0" :
+#     Activity_tool = Main_funtion()
 #     tki.Button(tk, text = "shutdown time", command = Activity_tool.shutdown_time).place(x = 105, y = 90)
 #     tki.Button(tk, text = "restart", command = Activity_tool.restart).place(x = 125, y = 120)
 #     tki.Button(tk, text = "cancel", command = Activity_tool.cancel).place(x = 125, y = 150)
@@ -57,12 +69,12 @@ button3 = tki.Button(tk, text = "cancel", command = Activity_tool.cancel).place(
 #     tki.Button(tk, text = "shutdown time", command = error_box()).place(x = 105, y = 90)
 #     tki.Button(tk, text = "restart", command = error_box()).place(x = 125, y = 120)
 #     tki.Button(tk, text = "cancel", command = error_box()).place(x = 125, y = 150)
-#     print(ent1.get())
+#     print(input_time_value.get())
 
     # error_box()
     
 
-# if ent1.get() <= "315360000" and ent1.get() >= "0" :
+# if input_time_value.get() <= "315360000" and input_time_value.get() >= "0" :
 #     button1['state'] = tki.ACTIVE
 #     button2['state'] = tki.ACTIVE
 #     button3['state'] = tki.ACTIVE
