e11186e668976b8f9f2b775144eadc2bb57ccf70
Anonymous <80660607+numpy-ai@users.noreply.github.com>
2022-03-11 00:19:02 +0900

    2022-03-11 #1
    범위 넘어가면 오류 뜨는 기능 추가, 0 아래로는 아직 추가 못함
    Test에서 적용했던 변수명 변경

--- shutdown.py
+++ shutdown.py
@@ -9,31 +9,51 @@ tk.geometry("300x200")
 tk.title("Shutdown program")
 tk.resizable(False, False)
 
-lab1 = tki.Label(tk)
-lab1['text'] = "희망하는 시간(Time you want)"
-lab1.pack()
+top_text = tki.Label(tk)
+top_text['text'] = "희망하는 시간(Time you want)"
+top_text.pack()
 limit_text_lab = tki.Label(tk)
 limit_text_lab['text'] = "Range : 0-315360000(10y)"
 limit_text_lab.pack()
 
-lab2 = tki.Label(tk)
-ent1 = tki.Entry(tk, textvariable=tki.IntVar())
-ent1.pack()
+Print_input_time_value = tki.Label(tk)
+input_time_value = tki.Entry(tk)
+input_time_value.insert(tki.INSERT, "Enter Time(s)")   
+input_time_value.pack()
 
 def print_result() :
-    lab2.configure(text = "shutdown a " + ent1.get() + "s after")
-lab2.pack()
+    Print_input_time_value.configure(text = "shutdown a " + input_time_value.get() + "s after")
+Print_input_time_value.pack()
 
 def error_box() :
     tkinter.messagebox.showerror("Limit Error!", "Range : 0-315360000(10y)")
 
+time_value_type = ""
+
+def time_value_number():
+    global time_value_type
+    try:
+        int(input_time_value.get())
+        time_value_type = "INT"
+    except ValueError:
+        time_value_type = "NOT INT"
+
+
 class Main_funtion : 
+    global time_value_type
+    time_value_number()
     def shutdown_time(self) : 
         print_result()
-        return os.system("shutdown -s -t " + ent1.get())
+        if time_value_type == "NOT INT" :
+            if int(input_time_value.get()) > 315360000 :
+                error_box()
+        return os.system("shutdown -s -t " + input_time_value.get())
     def restart(self) :
         print_result()
-        return os.system("shutdown -r -t " + ent1.get())
+        if time_value_type == "NOT INT" :
+            if int(input_time_value.get()) > 315360000 :
+                error_box()
+        return os.system("shutdown -r -t " + input_time_value.get())
     def cancel(self) :
         return os.system("shutdown -a")
 
@@ -41,6 +61,6 @@ Activity_tool = Main_funtion()
 
 button1 = tki.Button(tk, text = "shutdown time", command = Activity_tool.shutdown_time).place(x = 105, y = 90)
 button2 = tki.Button(tk, text = "restart", command = Activity_tool.restart).place(x = 125, y = 120)
-button3 = tki.Button(tk, text = "cancel", command = Activity_tool.cancel).place(x = 125, y = 150)
+button3 = tki.Button(tk, text = "cancel", command = Activity_tool.cancel).place(x = 125, y = 150)    
 
 tk.mainloop()
